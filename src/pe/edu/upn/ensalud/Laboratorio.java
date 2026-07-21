9061daae1194f15031c1d19cf51c3f772068deba
GANCITOS <losadaangel123@gmail.com>
2026-07-21 09:31:09 -0500

    Crecion de la clase Laboratorio

---
+++ src/pe/edu/upn/ensalud/Laboratorio.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upn.ensalud;
+
+/**
+ *
+ * @author Asus
+ */
+public class Laboratorio {
+    
+}
