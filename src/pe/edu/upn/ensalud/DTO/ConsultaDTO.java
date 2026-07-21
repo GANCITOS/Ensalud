ce2032d84151ec03377ad90036968d46a88cff8c
GANCITOS <losadaangel123@gmail.com>
2026-07-21 09:57:37 -0500

    Mejora de clases

--- src/pe/edu/upn/ensalud/DTO/ConsultaDTO.java
+++ src/pe/edu/upn/ensalud/DTO/ConsultaDTO.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package pe.edu.upn.ensalud;
+package pe.edu.upn.ensalud.DTO;
 
 /**
  *
  * @author Asus
  */
-public class Receta {
+public class ConsultaDTO {
     
 }
